ae079c15493b8503f0d2c631269c252e9c590584
Anh Truong <anh.truong.07@cnu.edu>
2016-07-28 09:46:19 -0400

    not working

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){ //client sends
-    io.emit('posting:message', msg); //broadcast
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
+
 
 http.listen(1337, function(){
   console.log('listening on 1337 skeet skeet')
